c07d899f8dba48aa8a752e7ea7cc3bafa440a1c2
Tanay Kamath <2tanaykamath@gmail.com>
2021-02-27 14:23:59 +0530

    add dice images :sparkles:

--- app/src/main/java/com/myapp/diceroll/MainActivity.kt
+++ app/src/main/java/com/myapp/diceroll/MainActivity.kt
@@ -2,6 +2,7 @@ package com.myapp.diceroll
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -22,8 +23,22 @@ class MainActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val dice = Dice(6)
+
         val rollText: TextView = findViewById(R.id.rollTextView)
-        rollText.text = dice.roll().toString()
+        val rollImg: ImageView = findViewById(R.id.diceImageView)
+
+        val roll: Int = dice.roll()
+        rollText.text = roll.toString()
+
+        when (roll) {
+            1 -> rollImg.setImageResource(R.drawable.dice_1)
+            2 -> rollImg.setImageResource(R.drawable.dice_2)
+            3 -> rollImg.setImageResource(R.drawable.dice_3)
+            4 -> rollImg.setImageResource(R.drawable.dice_4)
+            5 -> rollImg.setImageResource(R.drawable.dice_5)
+            6 -> rollImg.setImageResource(R.drawable.dice_6)
+        }
+
     }
 }
 
