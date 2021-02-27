4b0cfeb3ed6ac44c22fd50a64928cda7efbcaff2
Tanay Kamath <2tanaykamath@gmail.com>
2021-02-27 15:43:49 +0530

    add splash screen :sparkles:

---
+++ app/src/main/java/com/myapp/diceroll/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.myapp.diceroll
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        actionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+    }
+}
